c680549a64ecf7c0578fc08559c6c9459ddeba3b
littlestarling <takto78@gmail.com>
2019-02-09 21:13:21 +0900

    Move amount
      Moneyにamountを実装し、Dollarから除外する

--- app/dollar.rb
+++ app/dollar.rb
@@ -1,12 +1,6 @@
 # frozen_string_literal: true
 require_relative 'money'
 class Dollar < Money
-  attr_accessor :amount
-
-  def initialize(amount)
-    @amount = amount
-  end
-
   def times(multiplier)
     return Dollar.new(@amount * multiplier)
   end

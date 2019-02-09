58550cd0e99cf3e0640b514e70231620f85afebe
littlestarling <takto78@gmail.com>
2019-02-09 21:19:04 +0900

    eql?メソッドをMoneyに移動する

--- app/dollar.rb
+++ app/dollar.rb
@@ -8,13 +8,4 @@ class Dollar < Money
   def equals(obj)
     @amount == obj.amount
   end
-
-  # NOTE: 同値性比較する機能を提供する
-  def eql?(obj)
-    if obj.is_a?(Money)
-      @amount.eql?(obj.amount)
-    else
-      false
-    end
-  end
 end
--- app/money.rb
+++ app/money.rb
@@ -5,4 +5,13 @@ class Money
   def initialize(amount)
     @amount = amount
   end
+
+  # NOTE: 同値性比較する機能を提供する
+  def eql?(obj)
+    if obj.is_a?(Money)
+      @amount.eql?(obj.amount)
+    else
+      false
+    end
+  end
 end

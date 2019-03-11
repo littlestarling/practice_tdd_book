b2b06242cba71b5269adc705b9334a80e311f55d
littlestarling <takto78@gmail.com>
2019-03-11 22:08:28 +0900

    plusメソッドの検証テストの追加

--- spec/money_spec.rb
+++ spec/money_spec.rb
@@ -26,5 +26,12 @@ RSpec.describe 'Money' do
       expect(Money.franc(1).currency).to eq 'CHF'
     end
   end
+
+  describe 'addition' do
+    it 'returns simple added amount' do
+      sum = Money.dollar(5).plus(Money.dollar(5))
+      expect(sum).to eq(Money.dollar(10))
+    end
+  end
 end
 
